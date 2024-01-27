15f8ea3289974bdd36dba1ba39ba3cbfdba7fa23
MC_Spraint <snahashis@pietechsolution.com>
2024-01-27 18:07:10 +0530

    path

--- src/app/services/http/base.service.ts
+++ src/app/services/http/base.service.ts
@@ -1,16 +1,19 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class BaseService {
 
-  private baseUrl:string = "http://localhost:3000"
+  private baseUrl:string = environment.production 
+    ? "https://fresh-api.onrender.com" 
+    : "http://localhost:4200";
 
   constructor(private http: HttpClient) { }
 
   sendMessage(user:any){
-    return this.http.post<any>(this.baseUrl+"/contact",user)
+    return this.http.post<any>(this.baseUrl+"/api/contact/save",user);
   }
 }
